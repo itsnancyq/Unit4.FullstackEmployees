fc138982a9a0ba1bf47e3259864c9a25b33717f2
Nancy Quinonez <nancyq29@hotmail.com>
2025-05-20 20:56:56 -0500

    completed step 1

--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,5 @@
 import db from "#db/client";
+import { createEmployee } from "./queries/employees";
 
 await db.connect();
 await seedEmployees();
@@ -6,5 +7,9 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seedEmployees() {
-  // TODO
+  await client.connect()
+
+  await createEmployee("Spongebob", '1986-07-14', 1)
+
+  await client.end()
 }
